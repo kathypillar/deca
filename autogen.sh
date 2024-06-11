87adface04921f1d7096d844e58c5c02a5f68bf7
Katherine Halliday <Katherinehalliday256@gmail.com>
2024-06-10 16:40:54 -0800

    [Bug_Fix]:Forgot_a_file_oops

---
+++ autogen.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+# Generate configure & friends for GIT users.
+
+gnulib_url="git://git.sv.gnu.org/gnulib.git"
+gnulib_hash="f948d1813a3545c75ba79087e6cb52b51d44dfe6"
+
+modules="
+	canonicalize-lgpl
+	futimens
+	getdelim
+	getline
+	getopt-gnu
+	glob
+	isblank
+	iswblank
+	lstat
+	mkstemps
+	nl_langinfo
+	regex
+	sigaction
+	snprintf-posix
+	stdarg
+	strcase
+	strcasestr-simple
+	strnlen
+	sys_wait
+	vsnprintf-posix
+	wchar
+	wctype-h
+	wcwidth
+"
+
+# Make sure the local gnulib git repo is up-to-date.
+if [ ! -d "gnulib" ]; then
+	git clone --depth=2222 ${gnulib_url}
+fi
+cd gnulib >/dev/null || exit 1
+curr_hash=$(git log -1 --format=%H)
+if [ "${gnulib_hash}" != "${curr_hash}" ]; then
+	echo "Pulling..."
+	git pull
+	git checkout --force ${gnulib_hash}
+fi
+cd .. >/dev/null || exit 1
+
+echo "Autopoint..."
+autopoint --force
+
+rm -rf lib
+echo "Gnulib-tool..."
+./gnulib/gnulib-tool --import ${modules}
+echo
+
+echo "Aclocal..."
+aclocal -I m4
+echo "Autoconf..."
+autoconf
+echo "Autoheader..."
+autoheader
+echo "Automake..."
+automake --add-missing
+echo "Done."
